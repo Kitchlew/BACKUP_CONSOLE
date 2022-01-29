8cd212d3d9e30fa9692d589d542f8f09da897f3a
Shehreyar Kitchlew <62876048+Kitchlew@users.noreply.github.com>
2022-01-30 04:20:33 +0500

    yes

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\n\n\n\n\n\n\nPlease Copy the unique key provided below: \n\n\nddf9d9b8-9074-4efd-9cbe-2d52e1e6732f\n\n\n");
